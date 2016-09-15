afcb0ba8c4ab7b3322230b5cf17a433d46da0cad
Hien Dang <hienpd@outlook.com>
2016-09-15 10:04:07 -0700

    Fix overflow scrolling issue

--- app/components/NewGoal.jsx
+++ app/components/NewGoal.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
 
 const NewGoal = React.createClass({
   render() {
     return <div>
-      NewGoal
+      <h1>Set a New Goal</h1>
+      <Paper className="goal-form padding">
+        <TextField
+          floatingLabelText="Goal Name"
+          hintText="Learn how to tango"
+          floatingLabelFixed={true}
+          fullWidth={true}
+        /><br />
+        <TextField
+          floatingLabelText="Description"
+          hintText="The tango is a passionate dance and a great way to meet new people and get exercise. Learning how to tango is important to me because it fulfills a lifelong dream."
+          floatingLabelFixed={true}
+          fullWidth={true}
+          multiLine={true}
+          rows={2}
+        /><br />
+        <TextField
+          floatingLabelText="Add a Step"
+          hintText="Research dance instructors"
+          floatingLabelFixed={true}
+        />
+      </Paper>
     </div>;
   }
 });
