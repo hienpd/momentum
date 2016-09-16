ae784b1432c942c5430f706f4e8eca6812d3a45a
Hien Dang <hienpd@outlook.com>
2016-09-15 17:49:57 -0700

    Move save button to bottom right

--- app/components/NewGoal.jsx
+++ app/components/NewGoal.jsx
@@ -3,6 +3,7 @@ import Paper from 'material-ui/Paper';
 import NewGoalBasic from 'components/NewGoalBasic';
 import NewGoalFriends from 'components/NewGoalFriends';
 import NewGoalAdvanced from 'components/NewGoalAdvanced';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const NewGoal = React.createClass({
   render() {
@@ -15,6 +16,11 @@ const NewGoal = React.createClass({
         <div className="goal-form-col">
           <NewGoalFriends />
           <NewGoalAdvanced />
+          <RaisedButton
+            className="goal-save-btn"
+            label="Save Goal"
+            secondary={true}
+          />
         </div>
       </Paper>
     </div>;
