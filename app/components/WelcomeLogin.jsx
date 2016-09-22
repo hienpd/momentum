2c4e6c0a171259789bad8b9d1b96b882d5b16889
Hien Dang <hienpd@outlook.com>
2016-09-22 14:49:46 -0700

    Make things log in

--- app/components/WelcomeLogin.jsx
+++ app/components/WelcomeLogin.jsx
@@ -1,23 +1,68 @@
+import { Link, withRouter } from 'react-router';
+import axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
 
-const WelcomeLogin = () => (
-  <div className="container-cols">
-    <h3>Log in with your username and password</h3>
-    <TextField
-      floatingLabelText="Username"
-    />
-    <TextField
-      floatingLabelText="Password"
-      type="password"
-    />
-    <RaisedButton
-      label="Log In"
-      secondary={true}
-      className="margin"
-    />
-  </div>
-);
+const WelcomeLogin = React.createClass({
+  getInitialState() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
 
-export default WelcomeLogin;
+  handleUsername(event) {
+    const username = event.target.value;
+
+    this.setState({
+      username
+    });
+  },
+
+  handlePassword(event) {
+    const password = event.target.value;
+
+    this.setState({
+      password
+    });
+  },
+
+  handleLogin() {
+    axios.post('/api/token', {
+      username: this.state.username,
+      password: this.state.password
+    })
+    .then(() => {
+      this.props.router.push('/app');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  },
+
+  render() {
+    return <div className="container-cols">
+      <h3>Log in with your username and password</h3>
+      <TextField
+        floatingLabelText="Username"
+        value={this.state.username}
+        onChange={this.handleUsername}
+      />
+      <TextField
+        floatingLabelText="Password"
+        type="password"
+        value={this.state.password}
+        onChange={this.handlePassword}
+      />
+      <RaisedButton
+        label="Log In"
+        secondary={true}
+        className="margin"
+        onClick={this.handleLogin}
+      />
+    </div>;
+  }
+});
+
+export default withRouter(WelcomeLogin);
