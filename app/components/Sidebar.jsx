fe4722f582f66e4f58ae4ed21773ade52c476b1d
Hien Dang <hienpd@outlook.com>
2016-09-22 16:29:31 -0700

    Link pages

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,18 +1,89 @@
+import { Link, withRouter } from 'react-router';
+import axios from 'axios';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover';
 import React from 'react';
 
 const Sidebar = React.createClass({
+  getInitialState() {
+    return this.state = {
+      open: false
+    };
+  },
+
+  handleAvatarClick() {
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget
+    });
+  },
+
+  handleRequestClose() {
+    this.setState({
+      open: false
+    });
+  },
+
+  handleGoToSettings() {
+    this.props.router.push('/app/settings');
+  },
+
+  handleGoToNewGoal() {
+    this.props.router.push('/app/newgoal');
+  },
+
+  handleGoToFriends() {
+    this.props.router.push('/app/friends');
+  },
+
+  handleGoToDashboard() {
+    this.props.router.push('/app');
+  },
+
+  handleLogout() {
+    axios.delete('/api/token')
+      .then(() => {
+        this.props.router.push('/');
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      });
+  },
+
   render() {
     return <div className="container-sidebar-layout">
       <nav>
-        <div id="user-box">
+        <div id="user-box"
+          onClick={this.handleAvatarClick}
+        >
           <div id="avatar">
             <img className="avatar-img" src="http://media1.onsugar.com/files/2014/03/18/945/n/1922153/2d024105f59c4684_thumb_temp_image10906271395178611/i/Kerry-Washington-Makeup-Tutorial.jpg" />
           </div>
-          <h1 id="username">kerrywa</h1>
+          <h1 id="username">kaydub</h1>
+          <Popover
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu>
+              <MenuItem
+                primaryText="Settings"
+                onClick={this.handleGoToSettings}
+              />
+              <MenuItem
+                primaryText="Log Out"
+                onClick={this.handleLogout}
+              />
+            </Menu>
+          </Popover>
         </div>
         <ul id="sidebar-nav">
           <li>
-            <a href="#">
+            <a href="#" onClick={this.handleGoToDashboard}>
               <div className="icon-box">
                 <i className="fa fa-tachometer fa-lg" aria-hidden="true"></i>
               </div>
@@ -32,7 +103,7 @@ const Sidebar = React.createClass({
             <li><a href="#">Sample goal 2</a></li>
           </ul>
           <li>
-            <a href="#">
+            <a href="#" onClick={this.handleGoToNewGoal}>
               <div className="icon-box">
                 <i className="fa fa-plus fa-lg" aria-hidden="true"></i>
               </div>
@@ -40,7 +111,7 @@ const Sidebar = React.createClass({
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" onClick={this.handleGoToFriends}>
               <div className="icon-box">
                 <i className="fa fa-users fa-lf" aria-hidden="true"></i>
               </div>
@@ -48,9 +119,8 @@ const Sidebar = React.createClass({
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="#" onClick={this.handleGoToSettings}>
             <div className="icon-box">
-
               <i className="fa fa-cogs fa-lg" aria-hidden="true"></i>
               </div>
               Settings
@@ -65,4 +135,4 @@ const Sidebar = React.createClass({
   }
 });
 
-export default Sidebar;
+export default withRouter(Sidebar);
