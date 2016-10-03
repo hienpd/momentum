804dd7f18e94259988c805cdd73f9144435539b8
Hien Dang <hienpd@outlook.com>
2016-10-03 01:20:39 -0700

    Start inputs

--- app/components/NewGoal.jsx
+++ app/components/NewGoal.jsx
@@ -1,12 +1,38 @@
+import axios from 'axios';
+import cookie from 'react-cookie';
+import FontIcon from 'material-ui/FontIcon';
 import NewGoalAdvanced from 'components/NewGoalAdvanced';
 import NewGoalCategories from 'components/NewGoalCategories';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import TextField from 'material-ui/TextField';
-import FontIcon from 'material-ui/FontIcon';
 
 const NewGoal = React.createClass({
+  getInitialState() {
+    const userId = cookie.load('momentum_userId');
+
+    return {
+      goalName: '',
+      goalId: null,
+      userId: userId,
+      steps: []
+    };
+  },
+
+  handleSubmit() {
+    axios.post('/api/goals', {
+      goalName: this.state.goalName,
+      goalId: this.state.goalId
+    })
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  },
+
   render() {
     return <div>
       <h1>Set a New Goal</h1>
@@ -37,6 +63,7 @@ const NewGoal = React.createClass({
               className="save-btn"
               label="Save Goal"
               secondary={true}
+              onClick={this.handleSubmit}
             />
           </div>
         </div>
